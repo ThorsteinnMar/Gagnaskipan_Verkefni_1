0062fb02d04d22b1eeb8aee81f0dbb1388167a4c
jonstefan2 <jonstefan2@gmail.com>
2023-01-25 18:35:00 +0000

    með sma breytingum

--- array_list.py
+++ array_list.py
@@ -12,39 +12,51 @@ class NotOrdered(Exception):
 
 class ArrayList:
     def __init__(self, size):
-        self.arr = [0] * size
+        self.size = size
+        self.arr = [0] * self.size
         self.current_size = 0
+        self.last_num_index = -1
 
     def __str__(self):
-        if self.current_size == 0:
-            return ""
         string = ""
+        if self.current_size == 0:
+            return string
         for i in range(self.current_size):
             string += str(self.arr[i]) + ", "
-        return string[:-2]
+        return string[:-2]###breyta
     
     def prepend(self, value):
-        if self.current_size == len(self.arr):
+        if self.current_size == self.size:
+            self.add = True
             self.resize()
         for i in range(self.current_size, 0, -1):
             self.arr[i] = self.arr[i-1]
         self.arr[0] = value
         self.current_size += 1
+        self.last_num_index +=1
+        
         
     def insert(self, value, index):
-        if index < 0 or index > self.current_size:
+        if index < 0 or index > self.size-1:
             raise IndexOutOfBounds()
-        if self.current_size == len(self.arr):
+        if self.current_size == self.size:
+            self.add = True
             self.resize()
+        if index > self.last_num_index:
+            self.last_num_index = index
+        else:
+            self.last_num_index += 1
         for i in range(self.current_size, index, -1):
             self.arr[i] = self.arr[i-1]
         self.arr[index] = value
         self.current_size += 1
         
     def append(self, value):
-        if self.current_size == len(self.arr):
+        if self.current_size == self.last_num_index:
+            self.add = True
             self.resize()
-        self.arr[self.current_size] = value
+        self.arr[self.last_num_index + 1] = value
+        self.last_num_index += 1
         self.current_size += 1
 
     #Time complexity: O(1) - constant time
@@ -70,12 +82,35 @@ class ArrayList:
     #Time complexity: O(n) - linear time in size of list
     def resize(self):
         # TODO: remove 'pass' and implement functionality
-        pass
+        if self.add == True:
+            self.size += 1
+            temp_arr = [0] * self.size
+            for i in range(self.size-1):
+                temp_arr[i] = self.arr[i]
+        else:
+            self.size -= 1
+            temp_arr = [0] * self.size
+            index = 0
+            for i in range(self.size+1):
+
+                if self.arr[i] == "remove":
+                    continue
+                temp_arr[index] = self.arr[i]
+                index += 1
+            self.arr = temp_arr
+
+        #self.size+=1 eða self.size-=1
 
     #Time complexity: O(n) - linear time in size of list
     def remove_at(self, index):
         # TODO: remove 'pass' and implement functionality
-        pass
+        if index < 0 or index > self.size-1:
+            raise IndexOutOfBounds()
+        self.arr[index] = "remove"
+        self.current_size -= 1
+        self.add = False
+        self.last_num_index -= 1
+        self.resize()
 
     #Time complexity: O(1) - constant time
     def clear(self):
@@ -105,5 +140,12 @@ if __name__ == "__main__":
     # Do not add them outside this if statement
     # and make sure they are at this indent level
 
-    arr_lis = ArrayList()
+    arr_lis = ArrayList(5)
+    arr_lis.insert(4,0)
+    arr_lis.insert(9,1)
+    arr_lis.insert(1,2)
+    arr_lis.prepend(3)
+    arr_lis.remove_at(2)
+    arr_lis.remove_at(2)
+
     print(str(arr_lis))
